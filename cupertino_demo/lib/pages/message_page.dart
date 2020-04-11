1c389f0e903d5f20f342c450d2b79bece543aadd
李爱光 <aiguang.li@qq.com>
2020-04-11 16:45:32 +0800

    Add CupertinoPageScaffold demo
    1. TabPageScaffold
    2. CupertinoPageScaffold

---
+++ cupertino_demo/lib/pages/message_page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'cupertino_page_scaffold.dart';
+
+class HomePage extends StatelessWidget {
+	final String title;
+	HomePage({Key key, this.title});
+	
+	@override
+  	Widget build(BuildContext context) {
+    	// TODO: implement build
+    	return null;
+  	}
+}
