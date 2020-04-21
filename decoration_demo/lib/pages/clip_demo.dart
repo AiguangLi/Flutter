743a571868bea064c350b19bdcb7bfa37098a613
李爱光 <aiguang.li@qq.com>
2020-04-21 22:14:34 +0800

    Add round rectangle, path, rectable, clip demo

--- decoration_demo/lib/pages/clip_demo.dart
+++ decoration_demo/lib/pages/clip_demo.dart
@@ -1,6 +1,8 @@
 import 'package:decoration_demo/components/app_bar_factory.dart';
 import 'package:flutter/material.dart';
 
+enum ClipType { Oval, RoundRect, Path, Rect }
+
 class ClipDemo extends StatefulWidget {
   ClipDemo({Key key}) : super(key: key);
 
@@ -9,16 +11,125 @@ class ClipDemo extends StatefulWidget {
 }
 
 class _ClipDemoState extends State<ClipDemo> {
+  ClipType type = ClipType.Oval;
+  int typeIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBarFactory.buildCenterAppBar('Clip Demo'),
       body: Center(
-        child: ClipOval(
-          child: SizedBox(
-              child: Image.asset('images/villa.jpg', fit: BoxFit.fitHeight,), width: 300, height: 300),
-        ),
+        child: getClippedWidget(),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            type = ClipType.values[(typeIndex++) % ClipType.values.length];
+          });
+        },
+        child: Icon(Icons.refresh),
       ),
     );
+  }
+
+  Widget getClippedWidget() {
+    switch (type) {
+      case ClipType.Oval:
+        return getOvalClip();
+        break;
+      case ClipType.RoundRect:
+        return getRoundRectClip();
+        break;
+      case ClipType.Rect:
+        return getRectClip();
+        break;
+      case ClipType.Path:
+        return getPathClip();
+        break;
+      default:
+        return null;
+        break;
+    }
+  }
+
+  Widget getOvalClip() {
+    return ClipOval(
+      child: SizedBox(
+          child: Image.asset(
+            'images/villa.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+          width: 300,
+          height: 300),
+    );
+  }
+
+  Widget getRoundRectClip() {
+    return ClipRRect(
+      borderRadius: BorderRadius.all(Radius.circular(40.0)),
+      child: SizedBox(
+          child: Image.asset(
+            'images/villa.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+          width: 300,
+          height: 300),
+    );
+  }
+
+  Widget getRectClip() {
+    return ClipRect(
+      clipper: RectClipper(),
+      child: SizedBox(
+          child: Image.asset(
+            'images/villa.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+          width: 300,
+          height: 300),
+    );
+  }
+
+  Widget getPathClip() {
+    return ClipPath(
+      clipper: TriangleClipper(),
+      child: SizedBox(
+          child: Image.asset(
+            'images/villa.jpg',
+            fit: BoxFit.fitHeight,
+          ),
+          width: 300,
+          height: 300),
+    );
+  }
+}
+
+class RectClipper extends CustomClipper<Rect> {
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTWH(100, 100, 100, 100);
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Rect> oldClipper) {
+    return true;
+  }
+}
+
+class TriangleClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(100, 100);
+    path.lineTo(100, 200);
+    path.lineTo(200, 100);
+    path.close();
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) {
+    return true;
   }
 }
