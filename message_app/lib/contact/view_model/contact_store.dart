7993634cb7a92c147ce0bd9b48be10650453c775
李爱光 <aiguang.li@qq.com>
2020-05-14 18:46:13 +0800

    Feature: add contact_store

--- message_app/lib/contact/view_model/contact_store.dart
+++ message_app/lib/contact/view_model/contact_store.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/cupertino.dart';
-import 'package:message_app/message/service/message_service.dart';
-import 'package:message_app/message/message_model.dart';
-import 'package:message_app/models/list_pager.dart';
-import 'package:message_app/models/list_vo.dart';
-import '../../repository/global_service_repository.dart';
+import '../impl/contactor_service_impl.dart';
+import '../contactor_model.dart';
+import '../../models/list_pager.dart';
+import '../../models/list_vo.dart';
 
-class MessageStore with ChangeNotifier {
+class ContactorStore with ChangeNotifier {
   final BuildContext context;
-  MessageStore(this.context);
+  ContactorStore(this.context);
 
-  List<MessageModel> messageData;
+  List<ContactorModel> messageData;
   ListPager pager;
   bool isLoaded = false;
   bool hasMoreData = true;
 
   getListItems(int page, int pageSize, [Map<String, dynamic> params]) async {
     if (page == 1 || hasMoreData) {
-      ListVO<MessageModel> newData =
-          await GlobalServiceRepository.getService<MessageService>().listMessage(page, pageSize);
+      ListVO<ContactorModel> newData =
+          await ContactorServiceImpl.getSharedInstance().listContactor(page, pageSize);
       if (page == 1) {
         messageData = newData?.listItems;
       } else {
